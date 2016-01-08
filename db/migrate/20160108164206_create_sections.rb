1a6d46eb1d95f18161d96de154c8585e00587eab
Jay Yee <email": null>
2016-01-08 11:46:53 -0500

    Add items migration and edit sections migration

--- db/migrate/20160108164206_create_sections.rb
+++ db/migrate/20160108164206_create_sections.rb
@@ -2,7 +2,7 @@ class CreateSections < ActiveRecord::Migration
   def change
     create_table :sections do |t|
       t.references :menu, index: true, foreign_key: true
-      t.string :name, { null: false }
+      t.string :name, { null: false, limit: 64 }
 
       t.timestamps
     end
