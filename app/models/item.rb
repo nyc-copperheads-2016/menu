058a6ea021c6efe00805da19d92fbf6e171d8598
dijonkitchen <jschen2008@gmail.com>
2016-01-08 16:01:26 -0500

    Add taggability of Items by diets, tastes, and ingredients

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ActiveRecord::Base
   belongs_to :section
   has_many :favorites
   has_many :users, through: :favorites
+
+  acts_as_taggable
+  # Tag types
+  acts_as_taggable_on :diets, :tastes, :ingredients
 end
