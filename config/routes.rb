838be590a606003aaf0e9eb8d697e10f440d50f2
Aaron Tsai & Jonathan Chen <pair+aattsai.dijonkitchen@devbootcamp.com>
2016-01-09 16:21:18 -0500

    Add new user form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  # root 'restaurants#index'
+  root 'restaurants#index'
 
-  resources :users, only: [:create]
+  resources :users, only: [:new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
