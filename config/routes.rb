bb974ebd2fb62475959b21483ddc9c00317d194b
Aaron Tsai & Jonathan Chen <pair+aattsai.dijonkitchen@devbootcamp.com>
2016-01-09 16:44:32 -0500

    Add sessions controller and form

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   root 'restaurants#index'
 
   resources :users, only: [:new, :create]
+  resources :sessions, only:[:create]
+
+  get 'login' => 'sessions#new'
+  get 'logout'=> 'sessions#destroy'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
