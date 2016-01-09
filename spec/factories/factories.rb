ccb52eb0870d948e76923ee64c9d7f1f9e4fdbd7
Aaron Tsai & Jonathan Chen <pair+aattsai.dijonkitchen@devbootcamp.com>
2016-01-09 18:18:15 -0500

    Fix randomizatin of Faker data with curlies

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,42 +1,42 @@
 FactoryGirl.define do
   factory :user do
-    username   Faker::Internet.user_name
-    email      Faker::Internet.safe_email
+    username   { Faker::Internet.user_name }
+    email      { Faker::Internet.safe_email }
     password   'password'
   end
 
   factory :location do
-    address    Faker::Address.street_address
-    city       Faker::Address.city
-    state      Faker::Address.state_abbr
-    zip        Faker::Address.zip_code
-    latitude   Faker::Address.latitude
-    longitude  Faker::Address.longitude
+    address    { Faker::Address.street_address }
+    city       { Faker::Address.city }
+    state      { Faker::Address.state_abbr }
+    zip        { Faker::Address.zip_code }
+    latitude   { Faker::Address.latitude }
+    longitude  { Faker::Address.longitude }
     association :restaurant
   end
 
   factory :restaurant do
-    name        Faker::Company.name
-    website     Faker::Internet.url
-    description Faker::Company.bs
-    logo_url    Faker::Company.logo
+    name        { Faker::Company.name }
+    website     { Faker::Internet.url }
+    description { Faker::Company.bs }
+    logo_url    { Faker::Company.logo }
   end
 
   factory :menu do
-    name        Faker::University.name
+    name        { Faker::University.name }
     association :restaurant
   end
 
   factory :section do
-    name        Faker::Team.name
+    name        { Faker::Team.name }
     association :menu
   end
 
   factory :item do
-    name        Faker::Commerce.product_name
-    price       Faker::Commerce.price
-    photo_url   Faker::Company.logo
-    description Faker::Company.buzzword
+    name        { Faker::Commerce.product_name }
+    price       { Faker::Commerce.price }
+    photo_url   { Faker::Company.logo }
+    description { Faker::Company.buzzword }
     association :section
   end
 
