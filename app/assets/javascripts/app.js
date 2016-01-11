26cc0e4f0192ea46bbb54efb10b0852b2cd3558b
Eunice Do & Aaron Tsai <pair+Yoonis.aattsai@devbootcamp.com>
2016-01-11 14:44:33 -0500

    Add itemShow partial to ng-config route

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,8 +1,12 @@
 var myApp = angular.module('myApp', ['ngRoute']);
 
 myApp.config(function ($routeProvider, $locationProvider) {
-  $routeProvider.when('/restaurants/:restaurant_id/menuItems', {
+  $routeProvider
+  .when('/restaurants/:restaurant_id/menuItems', {
     templateUrl: '/partials/menuItems'
+  })
+  .when('/restaurants/:restaurant_id/menuItems/:item_id', {
+    templateUrl: '/partials/itemShow'
   })
   .otherwise({
     templateUrl: '/partials/restaurants'
