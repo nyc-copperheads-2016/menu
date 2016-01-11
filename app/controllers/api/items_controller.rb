09248312830a956921c1133fc726988360889100
Eunice Do & Aaron Tsai <pair+Yoonis.aattsai@devbootcamp.com>
2016-01-11 14:46:40 -0500

    Remove whitespace

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -2,16 +2,12 @@ class Api::ItemsController < ApplicationController
 
   def index
     restaurant = Restaurant.find_by(id: params[:restaurant_id])
-
     render json: restaurant.menus.first.items, include: ['ingredients', 'diets', 'tastes']
   end
 
   def show
     item = Item.find_by(id: params[:id])
-
     render json: item.to_json
   end
 
 end
-
-
