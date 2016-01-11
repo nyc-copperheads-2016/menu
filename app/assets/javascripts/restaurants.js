88266f4e9469465f5aa0d6127c4100148e14e222
Eunice Do & Aaron Tsai <pair+Yoonis.aattsai@devbootcamp.com>
2016-01-11 14:46:16 -0500

    Refactor

--- app/assets/javascripts/restaurants.js
+++ app/assets/javascripts/restaurants.js
@@ -1,12 +1,8 @@
-// Referencing the myApp module that was created in app.js.
-var myApp = angular.module('myApp');
-
 myApp.controller('RestaurantsController',
   ['$http', '$filter', '$scope', function($http, $filter, $scope) {
 
   $http.get('/api/restaurants')
   .then(function(response) {
-    console.log(response);
     $scope.restaurants = response.data;
   });
 }]);
