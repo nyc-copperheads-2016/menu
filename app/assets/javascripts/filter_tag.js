aac3434187de713129763594df9f4dc1d93c2c74
Jay Yee <email": null>
2016-01-10 21:49:04 -0500

    Add more edits to filter_tag

--- app/assets/javascripts/filter_tag.js
+++ app/assets/javascripts/filter_tag.js
@@ -3,20 +3,19 @@ myApp.controller('ItemsController',
     var params = $routeParams.restaurant_id;
     $http.get('http://localhost:3000/api/restaurants/'+params+'/items').then(function successCallback(response) {
     $scope.menuItems = response.data;
-    $scope.tagFilters = $scope.createTagFilters($scope.findUniqueTags(menuItems));
+    $scope.tagFilters = $scope.createTagFilters($scope.findUniqueTags());
     },
     function errorCallback(response) {
       console.log("There was an error");
   });
 
-  $scope.findUniqueTags = function(menuItems) {
+  $scope.findUniqueTags = function() {
     var tags = {};
-    angular.forEach(menuItems, function(menuItem) {
+    angular.forEach($scope.menuItems, function(menuItem) {
       angular.forEach(menuItem["diets"], function(tag) {
         tags[tag["name"]] = 1;
       });
     });
-    debugger
     /* return is an array of tag strings */
     return Object.keys(tags).sort();
   };
@@ -30,7 +29,6 @@ myApp.controller('ItemsController',
     return filters;
   };
 
-
   /* tagFilter is referencing one of the tag objects in the tagFilters variable. */
   $scope.toggleTagState = function(tagFilter) {
     tagFilter.state = (tagFilter.state + 1) % 3;
@@ -39,23 +37,16 @@ myApp.controller('ItemsController',
   /* menuItem references "item" in the HTML ng-repeat directive. Index and array are optional arguments that are not being used in customTagFilter. This fn is being invoked for each menu item, and it determines whether or not the item should be displayed. */
   $scope.customTagFilter = function(menuItem, index, array) {
     var showItem = true;
+    debugger
     /* Within the array of tagFilters tag objects... */
     angular.forEach($scope.tagFilters, function(tagFilter) {
       /* If tag state is 1 (clicked once) AND the menu item's tags do NOT contain the tag of state 1, then set showItem to false. */
-      // if (tagFilter.state == 1 && menuItem.diets.indexOf(tagFilter.name) == -1) {
-        // if the current object has a tag name == to the tag that was clicked on
-        // hide the item
-      debugger
-
-      menuItem.forEach(function (dietTag) {
-        // body...
-        if (tagFilter.state === 1 && dietTag.tag.forEach(function (tag)) { tag.name } !== tagFilter.name) {
-          showItem = false;
-        }
-        else if (tagFilter.state === 2 && tag.name === tagFilter.name) {
-          showItem = false;
-        }
-      })
+      if (tagFilter.state == 1 && menuItem.tags.indexOf(tagFilter.name) == -1) {
+        showItem = false;
+      }
+      else if (tagFilter.state == 2 && menuItem.tags.indexOf(tagFilter.name) > -1) {
+        showItem = false;
+      }
     });
     return showItem;
   };
