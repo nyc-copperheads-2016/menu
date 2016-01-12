39cbe96e30c0c7e80118d497f04612a98bd866d1
Eunice Do & Aaron Tsai <pair+Yoonis.aattsai@devbootcamp.com>
2016-01-12 14:17:17 -0500

    Add unfavorite API call and Delete call

--- app/assets/javascripts/favorites.js
+++ app/assets/javascripts/favorites.js
@@ -6,14 +6,24 @@ myApp.controller('FavoritesController',
   $scope.saveFave = function() {
     if ($scope.heartClass === "fi-heart unliked") {
       $scope.heartClass = "fi-heart liked";
+      $http({
+        url: '/favorites',
+        method: "POST",
+        data: {'item_id': itemParam}
+      })
     }
     else {
       $scope.heartClass = "fi-heart unliked";
+      $http.get('/api/restaurants/' + restaurantParam + '/items/' + itemParam)
+      .then(function successCallback(response) {
+        $scope.item = response.data;
+        var faveId = $scope.item[1].id
+        $http({
+          url: '/favorites/' + faveId,
+          method: "DELETE",
+          data: {'item_id': itemParam}
+        })
+      });
     };
-    $http({
-    url: '/favorites',
-    method: "POST",
-    data: {'restaurant_id': restaurantParam, 'item_id': itemParam}
-    })
   }
 }]);
