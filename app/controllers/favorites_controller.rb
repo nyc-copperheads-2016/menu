78c6f835a86481694c1a7863c36de419a3c24778
Eunice Do & Aaron Tsai <pair+Yoonis.aattsai@devbootcamp.com>
2016-01-12 15:47:17 -0500

    Replaced find_by with find

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,13 +5,18 @@ class FavoritesController < ApplicationController
     fave = current_user.favorites.build(item_id: params[:item_id])
     if fave.save
       flash[:notice] = "Saved to favorites!"
+    else
+      flash[:notice] = "Unable to add to favorites. Please try again later!"
     end
   end
 
   def destroy
-    fave = Favorite.find_by(id: params[:id])
+    fave = Favorite.find(params[:id])
     if fave.destroy
       flash[:notice] = "Removed from favorites!"
+    else
+      flash[:notice] = "Unable to remove from favorites"
     end
+
   end
 end
