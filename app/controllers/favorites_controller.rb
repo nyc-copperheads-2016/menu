f1814ba7663a823aa9ea6caef6013bda84ec316d
Eunice Do & Aaron Tsai <pair+Yoonis.aattsai@devbootcamp.com>
2016-01-12 16:01:22 -0500

    Change flash messages to console message

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,18 +4,18 @@ class FavoritesController < ApplicationController
   def create
     fave = current_user.favorites.build(item_id: params[:item_id])
     if fave.save
-      flash[:notice] = "Saved to favorites!"
+      puts "Saved to favorites!"
     else
-      flash[:notice] = "Unable to add to favorites. Please try again later!"
+      puts "Unable to add to favorites. Please try again later!"
     end
   end
 
   def destroy
     fave = Favorite.find(params[:id])
     if fave.destroy
-      flash[:notice] = "Removed from favorites!"
+      puts "Removed from favorites!"
     else
-      flash[:notice] = "Unable to remove from favorites"
+      puts "Unable to remove from favorites"
     end
 
   end
