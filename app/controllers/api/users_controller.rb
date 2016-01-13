c61082668830f08bbc890783c41de1480f212073
Aaron Tsai <email": null>
2016-01-13 18:39:06 -0500

    Format json for user show page

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,18 @@
 class Api::UsersController < ApplicationController
 
   def show
-    render json: current_user.to_json
+    render json: current_user, include: [:items => { :include =>
+      [:section => { :include =>
+      [:menu => { :include =>
+      [:restaurant]
+    }]}]}]
   end
 
 end
+
+
+# include: [:menus => { :include =>
+#       [:sections => { :include =>
+#         [:items => { :include =>
+#           [:diets, :ingredients, :tastes, :favorites]
+#         }]
