4e6c8f2a9ab1243339cb66e36f9ee89954186cd3
Aaron Tsai <aattsai@gmail.com>
2016-01-13 10:09:45 -0500

    Fix minor bug on the dead rabbit seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -133,86 +133,87 @@ m_j.taste_list = "sweet, creamy"
 m_j.ingredient_list = "whiskey, egg, bitters"
 m_j.save
 
-w_g.diet_list = "sour, creamy"
+w_g.taste_list = "sour, creamy"
 w_g.ingredient_list = "whiskey, bitters"
 w_g.save
 
-m.diet_list = "sweet, creamy"
+m.taste_list = "sweet, creamy"
 m.ingredient_list = "sherry, bitters, vanilla"
 m.save
 
-p_m.diet_list = "sweet, heavy, bitter"
+p_m.taste_list = "sweet, heavy, bitter"
 p_m.ingredient_list = "whiskey, tequila, bitters ,vermouth"
 p_m.save
 
-b_j.diet_list = "creamy, bitter"
+b_j.taste_list = "creamy, bitter"
 b_j.ingredient_list = "whiskey, bitters"
 b_j.save
 
-b_r.diet_list = "aromatic, bitter, sweet"
+b_r.taste_list = "aromatic, bitter, sweet"
 b_r.ingredient_list = "whiskey, black tea, bitters"
 b_r.save
 
-l_w.diet_list = "heavy, sweet, aromatic"
+l_w.taste_list = "heavy, sweet, aromatic"
 l_w.ingredient_list = 'gin, chartreuse, absinthe'
 l_w.save
 
-#===============================================================================
-# restaurants_with_menus = Curl.post("https://api.locu.com/v2/venue/search", {
-#   api_key: Rails.application.secrets.locu_api_key,
-#   fields: [
-#     "locu_id",
-#     "name"
-#   ],
-#   venue_queries: [
-#   {
-#     location: {
-#       geo: {
-#         '$in_lat_lng_radius'.to_sym => [40.7061972,-74.0123236, 200]
-#       }
-#     },
-#     menus: { '$present'.to_sym => true }
-#   }
-# ]}.to_json)
+# ===============================================================================
 
-# venues = JSON.parse(restaurants_with_menus.body_str)["venues"]
+restaurants_with_menus = Curl.post("https://api.locu.com/v2/venue/search", {
+  api_key: Rails.application.secrets.locu_api_key,
+  fields: [
+    "locu_id",
+    "name"
+  ],
+  venue_queries: [
+  {
+    location: {
+      geo: {
+        '$in_lat_lng_radius'.to_sym => [40.7061972,-74.0123236, 200]
+      }
+    },
+    menus: { '$present'.to_sym => true }
+  }
+]}.to_json)
 
-# venues.each do |v|
-#   r = Restaurant.create!(name: v['name'])
+venues = JSON.parse(restaurants_with_menus.body_str)["venues"]
 
-#   r_details_json = Curl.post("https://api.locu.com/v2/venue/search", {
-#     api_key: Rails.application.secrets.locu_api_key,
-#     fields: [
-#       "name",
-#       "menus",
-#       "categories"
-#     ],
-#   venue_queries: [{ locu_id: v["locu_id"] }]}.to_json)
+venues.each do |v|
+  r = Restaurant.create!(name: v['name'])
 
-#   menus = JSON.parse(r_details_json.body_str)['venues'].first['menus']
+  r_details_json = Curl.post("https://api.locu.com/v2/venue/search", {
+    api_key: Rails.application.secrets.locu_api_key,
+    fields: [
+      "name",
+      "menus",
+      "categories"
+    ],
+  venue_queries: [{ locu_id: v["locu_id"] }]}.to_json)
 
-#   menus.each do |menu|
-#     m = r.menus.create!(name: menu['menu_name'])
-#     menu['sections'].each do |section|
-#       section['subsections'].each do |subsection|
-#         if !(subsection['subsection_name']).blank?
-#           s = m.sections.create!(name: subsection['subsection_name'])
-#           subsection['contents'].each do |content|
-#             if !(content['name'].blank?)
-#               i = s.items.create!(name: content['name'],
-#                                      price: content['price'],
-#                                      description: content['description'])
-#             else
-#               puts "Skipping blank content"
-#             end
-#           end
-#         else
-#           puts "Not saving blank subsection"
-#         end
-#       end
-#     end
-#   end
-# end
+  menus = JSON.parse(r_details_json.body_str)['venues'].first['menus']
+
+  menus.each do |menu|
+    m = r.menus.create!(name: menu['menu_name'])
+    menu['sections'].each do |section|
+      section['subsections'].each do |subsection|
+        if !(subsection['subsection_name']).blank?
+          s = m.sections.create!(name: subsection['subsection_name'])
+          subsection['contents'].each do |content|
+            if !(content['name'].blank?)
+              i = s.items.create!(name: content['name'],
+                                     price: content['price'],
+                                     description: content['description'])
+            else
+              puts "Skipping blank content"
+            end
+          end
+        else
+          puts "Not saving blank subsection"
+        end
+      end
+    end
+  end
+end
 
 User.create(username: "admin", email: "admin@example.com", password: "asdfasdf")
 
