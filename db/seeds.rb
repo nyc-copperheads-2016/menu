022cdc01ad1d552a9dcb379f52ead70be8a12c5d
Aaron Tsai <aattsai@gmail.com>
2016-01-14 11:07:14 -0500

    New photo for clam chowder

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,7 @@ sweets = m.sections.create(name: "Raw Bar")
 lobster = main.items.create(name: "Lobster Roll", price: "16.00", photo_url: "http://s3-media3.fl.yelpcdn.com/bphoto/ZrnAsssJfSJMjWepQKXP9w/258s.jpg")
 crab = main.items.create(name: "Crab Roll", price: "13.00", photo_url: "http://s3-media2.fl.yelpcdn.com/bphoto/1yUPo3-t0OMQwcquM8Swng/258s.jpg")
 
-clam = extras.items.create(name: "New England Clam Chowder", price: "9.00", photo_url: "http://s3-media4.fl.yelpcdn.com/bphoto/bHTtpvjf5eYRmUHKBQsSow/258s.jpg")
+clam = extras.items.create(name: "New England Clam Chowder", price: "9.00", photo_url: "http://captaincrab.org/wp-content/uploads/2015/06/chowder-large.jpg")
 claws = extras.items.create(name: "4 Jonah Crab Claws", price: "8.00", photo_url: "http://s3-media3.fl.yelpcdn.com/bphoto/ZrnAsssJfSJMjWepQKXP9w/258s.jpg")
 
 mainebrew = drinks.items.create(name: "Maine Microbrews", price: "6.50", photo_url: "http://s3-media4.fl.yelpcdn.com/bphoto/5L6AXcbZlNFxhTbQ0UW9Kw/258s.jpg")
