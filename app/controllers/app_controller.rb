a9cea19576ea08557a18537e907b620096638437
Aaron Tsai <email": null>
2016-01-13 20:32:52 -0500

    Remove uncessary files and routes

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,19 +1,2 @@
 class AppController < ApplicationController
-
-  def index
-  end
-
-  def restaurants
-    render 'app/index'
-  end
-
-  def menuItems
-  end
-
-  def itemShow
-  end
-
-  def userShow
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get 'partials/restaurants' => 'partials#restaurants'
   get 'partials/userShow' => 'partials#userShow'
 
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:create]
   resources :sessions, only:[:create]
 
   namespace :api do
