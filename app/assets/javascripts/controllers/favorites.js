fe2ae295014e6bc105c5a0804ab0331cbb408866
Jack Huang <Jack7100@gmail.com>
2016-01-25 22:00:11 -0500

    Indent for function

--- app/assets/javascripts/controllers/favorites.js
+++ app/assets/javascripts/controllers/favorites.js
@@ -1,49 +1,50 @@
 myApp.controller('FavoritesController',
   [ '$scope', '$routeParams', '$http', '$window', function FaveCtrl($scope, $routeParams, $http, $window) {
-  var restaurantParam = $routeParams.restaurant_id;
-  var itemParam = $routeParams.item_id;
+    var restaurantParam = $routeParams.restaurant_id;
+    var itemParam = $routeParams.item_id;
 
-  $http.get('/api/restaurants/' + restaurantParam + '/items/' + itemParam)
-  .then(function successCallback(response) {
-    $scope.item = response.data;
-
-    if ($scope.item[1] === null) {
-      $scope.heartClass = "fi-heart unliked";
-    }
-    else {
-      $scope.heartClass = "fi-heart liked";
-    }
-  });
-
-  $scope.menuBack = function() {
-    $window.history.back();
-  };
-
-  $scope.saveFave = function() {
     $http.get('/api/restaurants/' + restaurantParam + '/items/' + itemParam)
     .then(function successCallback(response) {
       $scope.item = response.data;
 
       if ($scope.item[1] === null) {
-        $scope.heartClass = "fi-heart liked";
-        $http({
-          url: '/favorites',
-          method: "POST",
-          data: {'item_id': itemParam}
-        });
-      }
-      else if ($scope.item[1] === '/register'){
-        $window.location.href = '/register';
+        $scope.heartClass = "fi-heart unliked";
       }
       else {
-        $scope.heartClass = "fi-heart unliked";
-        var faveId = $scope.item[1].id;
-        $http({
-          url: '/favorites/' + faveId,
-          method: "DELETE",
-          data: {'item_id': itemParam}
-        });
+        $scope.heartClass = "fi-heart liked";
       }
     });
-  };
-}]);
+
+    $scope.menuBack = function() {
+      $window.history.back();
+    };
+
+    $scope.saveFave = function() {
+      $http.get('/api/restaurants/' + restaurantParam + '/items/' + itemParam)
+      .then(function successCallback(response) {
+        $scope.item = response.data;
+
+        if ($scope.item[1] === null) {
+          $scope.heartClass = "fi-heart liked";
+          $http({
+            url: '/favorites',
+            method: "POST",
+            data: {'item_id': itemParam}
+          });
+        }
+        else if ($scope.item[1] === '/register'){
+          $window.location.href = '/register';
+        }
+        else {
+          $scope.heartClass = "fi-heart unliked";
+          var faveId = $scope.item[1].id;
+          $http({
+            url: '/favorites/' + faveId,
+            method: "DELETE",
+            data: {'item_id': itemParam}
+          });
+        }
+      });
+    };
+  }
+]);
