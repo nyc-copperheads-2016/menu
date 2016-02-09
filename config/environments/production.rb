e40eda42fa7de8efa6bbf8172f79751eae14a90f
Eunice Do <eunicedx@gmail.com>
2016-02-08 20:48:46 -0500

    Configure Rails to set secret key using ENV var.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Sets the secret key using the environment variable instead of looking for it in secrets.yml.
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
