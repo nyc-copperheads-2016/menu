2d51990c12e1edae26be49003ed3e0d244259a16
dijonkitchen <jschen2008@gmail.com>
2016-03-18 16:55:09 -0400

    Revert "remove comments"
    This reverts commit 0bc02b5bf3fc9e7b7eac23fc80a00e22ee8aa7ea.

--- app/assets/javascripts/custom_filter.js
+++ app/assets/javascripts/custom_filter.js
@@ -1,47 +1,65 @@
 myApp.filter('matchClickedItems', function() {
   return function(items, tagFilters) {
 
+    // Checks for tags that are clicked once aka inclusive search
     var includes = tagFilters.filter(function(tag) {
       return tag.state === 1;
     });
 
+    // Checks for tags that are clicked twice aka exclusive search
     var excludes = tagFilters.filter(function(tag) {
       return tag.state === 2;
     });
 
+    // Checks if an item
     function hasProperty(item, propName, propValue) {
+      // If no such tag type exists for an item, return false
       if (!item[propName]) {
         console.log('missing prop ' + propName , item);
         return false;
       }
+      // Otherwise, checks in a tag type for a tag name that is the same as compared tag name
       var filtered = item[propName].filter(function(ing) {
+        // console.log('ing', ing);
+        // console.log('pv', propValue);
         return ing.name === propValue;
       });
+      // Return if filtered if any found
       return filtered.length > 0;
     }
 
+    // Checks if an item has a tag in the ingredients tag type
     function hasIngredient(item, name) {
       return hasProperty(item, 'ingredients', name);
     }
 
+    // Checks if an item has a tag in the diets tag type
     function hasDiet(item, name) {
       return hasProperty(item, 'diets', name);
     }
 
+    // Checks if an item has a tag in the tastes tag type
     function hasTaste(item, name) {
       return hasProperty(item, 'tastes', name);
     }
 
 
     function includeItem(item) {
+      // Default to true aka always show item
       result = true;
+      // If a tag is selected for inclusive search,
       if (includes.length) {
+        // Default to false aka don't show item
         result = false;
+        // Iterate through each inclusive tag, and search in each tag type
         includes.forEach(function(inc) {
+          // Check if item has that tag by checking within the tagtype of the item
           result = result || hasIngredient(item, inc.name) || hasDiet(item, inc.name) || hasTaste(item, inc.name);
         });
       }
+      // If the item does have the tag, and there are other tags marked for excluding items
       if (result && excludes.length) {
+        // Check to see if the item should be excluded by any other filter
         excludes.forEach(function(excl) {
           result = result && !(hasIngredient(item, excl.name) || hasDiet(item, excl.name) || hasTaste(item, excl.name));
         });
@@ -50,6 +68,7 @@ myApp.filter('matchClickedItems', function() {
     }
 
     var filtered = [];
+    // Take the items in the ng-repeat and filter them
     filtered = items.filter(function(item){
       return includeItem(item);
     });
